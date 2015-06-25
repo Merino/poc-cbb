486a6050aaaf7a4fc985fbba91822ca10540619f
Merijn Bertels <merijn.bertels@gmail.com>
2015-06-25 19:17:20 +0200

    refactor new layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -12,30 +13,19 @@ module.exports = function(grunt) {
         dest: 'panels/static/javascript/panels.js'
       }
     },
-    compass: {
-      dist: {
-        options: {
-          sassDir: ['panels/source/apps/css', 'panels/source/vendors/'],
-          fontsPath: 'panels/source/vendors/bootstrap/assets/fonts/',
-          cssDir: 'panels/static/css',
-          fontsDir: 'panels/static/fonts',
-          imagesDir: 'panels/static/images',
-          javascriptsDir: 'panels/static/javascript/',
-          outputStyle: 'compressed',
-          relativeAssets: true,
-          noLineComments: false,
+
+    sass: {
+        dist: {
+          options: {
+            style: 'expanded'
+          },
+          files: {                         // Dictionary of files
+            'panels/static/css/apps.css': 'panels/source/apps/css/apps.scss',
+            'panels/static/css/libs.css': 'panels/source/vendors/libs.scss'
+          }
         }
-      }
     },
-//    concat: {
-//        dist: {
-//            files: {
-//                'panels/static/css/libs.css': [
-//                    'panels/source/vendors/font-awesome/css/font-awesome.min.css',
-//                ]
-//            }
-//        }
-//    },
+
     copy: {
         fonts: {
             expand: true,
@@ -52,13 +42,12 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'compass', 'copy']);
-
+  grunt.registerTask('default', ['uglify', 'sass', 'copy']);
 
   grunt.registerTask('build', ['uglify']);
 };
